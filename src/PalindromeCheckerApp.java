4227e6dbf378e976c9994fa69c04642874942f37
sheetam522 <sheetambiswal@gmail.com>
2026-02-25 15:08:19 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   WELCOME TO PALINDROME APP     ");
+        System.out.println("=================================");
+
+    }
+}
